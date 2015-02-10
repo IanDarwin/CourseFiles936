ea96e53bbca7e7ee05ddda6390ae609ed36eb5ec
Ian Darwin <ian@darwinsys.com>
2015-02-10 16:29:22 -0500

    DOS file to install on the other side.

---
+++ setup.bat
@@ -0,0 +1,15 @@
+c:
+cd \
+jar xvf e:\CourseFiles.zip
+
+jar xvf e:\TicketManorJava.zip
+
+cd CourseFiles\sourcecode
+
+jar xvf e:\jsfdemo.zip
+
+jar xvf e:\jpademo.zip
+
+jar xvf e:\darwinsys-api.zip
+
+jar xvf e:\javasrc.zip
