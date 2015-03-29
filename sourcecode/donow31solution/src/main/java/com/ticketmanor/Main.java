d9e45fe924618bfd2965e16bfa4d30fa967bc90b
Ian Darwin <ian@darwinsys.com>
2015-03-29 10:29:06 -0400

    Marked up for makehandsons

--- sourcecode/donow31solution/src/main/java/com/ticketmanor/Main.java
+++ sourcecode/donow31solution/src/main/java/com/ticketmanor/Main.java
@@ -10,12 +10,14 @@ public class Main {
 	/** Create a Customer object and save into the database */
 	public static void main(String[] args) {
 
-		// Set up JPA as per JavaSE way of doing things
+		//T Set up JPA as per JavaSE way of doing things
+		//-
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("donow31solution");
 		EntityManager em = emf.createEntityManager();
+		//+
 
 		// Create customer object
-		Customer customer = new Customer();
+		Customer customer = new Customer(), customer2 = null;
 		customer.setFirstName("John");
 		customer.setLastName("Doe");
 
@@ -23,8 +25,10 @@ public class Main {
 		EntityTransaction entityTransaction = em.getTransaction();
 		entityTransaction.begin();
 
-		// Now save the object
+		//T Now save the object
+		//-
 		em.persist(customer);
+		//+
 
 		// And commit the transaction
 		entityTransaction.commit();
@@ -32,24 +36,30 @@ public class Main {
 		// All done. But let's read it back to verify
 		long id = customer.getId();
 		
-		// Let's get a new EntityManager
+		//T Let's set "em" to a newly-created EntityManager, use it to "find" the Customer object
+		// identified by "id", and save it in the variable "customer2" declared above
+		//-
 		em = emf.createEntityManager();
-		Customer customer2 = em.find(Customer.class, id);
+		customer2 = em.find(Customer.class, id);
+		//+
 
-		// Check that they're the same
+		// Now we'll check that they're the same
 		if (customer.getName().equals(customer2.getName())) {
 			System.out.println("They match");
 		} else {
 			System.out.println("Arrgghh! They don't match");
 		}
 
-		// Now count how many Customers objects there are
-		Long n = em.createQuery("select count(c) from Customer c", Long.class).getSingleResult();
+		// Now let's count how many Customers objects there are
+		String query = null;
+		//T Assign to "query" a JPQL command that will "select" the count of c from Customer c
+		//-
+		query = "select count(c) from Customer c";
+		//+
+		Long n = em.createQuery(query, Long.class).getSingleResult();
 		System.out.printf("There are %d Customers%n", n);
 		
 		// All done? Close the EntityManager
 		em.close();
-		
-		//+
 	}
 }
