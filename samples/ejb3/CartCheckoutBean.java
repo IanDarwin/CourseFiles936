be2f2e1611ed8cf69315eec9327d71a2e2e237ee
Ian Darwin <ian@darwinsys.com>
2015-04-21 23:01:17 -0400

    Moar

---
+++ samples/ejb3/CartCheckoutBean.java
@@ -0,0 +1,34 @@
+import javax.ejb.*;
+import java.util.List;
+
+@Stateful
+public class CartCheckoutBean {
+	@Stateless public CreditCardValidationBean validator;
+
+	private List<Item> cart;
+
+	public boolean checkout(Customer) {
+		if (validator.validate(customer)) {
+			// process items in "cart" to complete order
+			return true;
+		}
+		return false;
+	}
+	
+	/** A @Remove method is needed to "disconnect" from a SFSB
+	@Remove
+	public void cleanup() {
+		// empty
+	}
+
+	static class Item {
+		// Just to make the above compile
+	}
+
+	static class CreditCardValidationBean {
+		boolean Validate(Object o) {
+			return false;	// just to make it compile
+		}
+	}
+}
+
---
+++ samples/ejb3/MyMDB.java
@@ -0,0 +1,18 @@
+import javax.ejb.*;
+import javax.jms.MessageListener;
+
+@MessageDriven(mappedName="jms/MyQueue", activationConfig =  {
+	@ActivationConfigProperty(propertyName = "destinationType", 	propertyValue = "javax.jms.Queue"),
+	@ActivationConfigProperty(propertyName = "acknowledgeMode", 	propertyValue = "Auto-acknowledge")
+})
+public class MyMDB implements MessageListener {
+	public void onMessage(Message m) {
+		if (m instanceof TextMessage) {
+			String text = ((TextMessage)m).getText();
+			// do something with "text" - log it, store in database, etc.
+		} else {
+			throw new IllegalArgumentException(
+			"Expected TextMessage, got " + m.getClass().getName());
+		}
+	}
+}
