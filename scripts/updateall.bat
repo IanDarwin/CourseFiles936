6b76ea8a99630e8ba8bb2e32ed16631be6e8346e
Chris Mawata <chris@mawata.org>
2015-05-03 14:21:39 -0400

    new

---
+++ scripts/updateall.bat
@@ -0,0 +1,31 @@
+cd \makehandsons
+git pull
+call mvn install
+
+
+cd \TicketManorJava
+ git pull
+
+cd \CourseFiles
+ git pull
+
+cd SourceCode
+
+rm -r ex?? donow??
+
+
+call \makehandsons\scripts\makehandsons.cmd .
+
+
+cd \jpademo
+ git pull
+
+cd \jsfdemo
+ git pull
+
+cd \javasrc
+ git pull
+
+
+cd \clublist
+ git pull
