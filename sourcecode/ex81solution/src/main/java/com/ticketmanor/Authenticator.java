87b986cba2d2891b6015b5d11f9863a85e7e9030
Ian <ian@darwinsys.com>
2016-02-14 15:26:22 -0500

    JSF can only find @ManagedBean, not @Named - likely a bug in WF8.x

--- sourcecode/ex81solution/src/main/java/com/ticketmanor/Authenticator.java
+++ sourcecode/ex81solution/src/main/java/com/ticketmanor/Authenticator.java
@@ -1,21 +1,23 @@
 package com.ticketmanor;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-@Named("authenticator")
+@ManagedBean
 public class Authenticator {
 
-	public void logout() {
+	public String logout() {
 		FacesContext ctx = FacesContext.getCurrentInstance();
 		try {
 			((HttpServletRequest)ctx.getExternalContext().getRequest()).logout();
+			ctx.addMessage(null, new FacesMessage("You have been logged out."));
 		} catch (ServletException e) {
 			System.out.println("How odd: logout failed");
 			ctx.addMessage(null, new FacesMessage("Logout failed."));
 		}
+		return "/";
 	}
 }
