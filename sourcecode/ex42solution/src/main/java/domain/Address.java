29d0dcf9ef83901f227c73df317f1f9b47804a6d
Ian Darwin <ian@darwinsys.com>
2017-12-20 14:28:32 -0500

    Let a thousand Persons blossom, err, compile

---
+++ sourcecode/ex42solution/src/main/java/domain/Address.java
@@ -0,0 +1,52 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class Address {
+	
+	private int id;
+	
+	@NotEmpty
+	private String streetAddress;
+	@NotEmpty
+	private String city;
+	private Country country = Country.CANADA;
+
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Country getCountry() {
+		return country;
+	}
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	
+	@Override
+	public String toString() {
+		return id + ": " + streetAddress + ", " + city;
+	}
+}
--- sourcecode/ex42solution/src/main/java/domain/Person.java
+++ sourcecode/ex42solution/src/main/java/domain/Person.java
@@ -28,7 +28,6 @@ public class Person {
 	protected String firstName;
 	protected String lastName;
 	int version;
-	Map<PrefType,Preference> prefs;
 	// Used by JPA with SINGLE_TABLE mode; not really
 	// part of the data model; just exposed here so that
 	// import.sql will work for any inheritance strategy.
@@ -104,7 +103,6 @@ public class Person {
 				+ ((firstName == null) ? 0 : firstName.hashCode());
 		result = prime * result
 				+ ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((prefs == null) ? 0 : prefs.hashCode());
 		return result;
 	}
 
@@ -127,11 +125,6 @@ public class Person {
 				return false;
 		} else if (!lastName.equals(other.lastName))
 			return false;
-		if (prefs == null) {
-			if (other.prefs != null)
-				return false;
-		} else if (!prefs.equals(other.prefs))
-			return false;
 		return true;
 	}
 	
@@ -139,16 +132,4 @@ public class Person {
 	public String toString() {
 		return getName() + "#" + getId();
 	}
-
-	// XXX This should probably be in Customer
-	@OneToMany(mappedBy="person")
-	@MapKey(name="prefType")
-	public Map<PrefType, Preference> getPrefs() {
-		return prefs;
-	}
-
-	public void setPrefs(Map<PrefType, Preference> prefs) {
-		this.prefs = prefs;
-	}
-	
 }
