7df7cca69c8b5399f6df19763dd2c6b134460abd
Ian Darwin <ian@darwinsys.com>
2017-12-22 20:51:51 -0500

    No-arg constr logs itself; add @Priority

--- sourcecode/ex61solution/src/main/java/com/ticketmanor/service/LoggingInterceptor.java
+++ sourcecode/ex61solution/src/main/java/com/ticketmanor/service/LoggingInterceptor.java
@@ -1,12 +1,17 @@
 package com.ticketmanor.service;
 
+import javax.annotation.Priority;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
-import javax.interceptor.Interceptors;
 import javax.interceptor.InvocationContext;
 
 @Interceptor
+@Priority(Interceptor.Priority.APPLICATION)
 public class LoggingInterceptor {
+
+	public LoggingInterceptor() {
+		System.out.println("LoggingInterceptor.init()");
+	}
 
 	// @AroundInvoke applies to business method; see also @AroundTimeout for timeout methods, etc.
 	@AroundInvoke
@@ -17,6 +22,9 @@ public class LoggingInterceptor {
 		return o;
 	}
 	
+	// Following is how it would normally be used; commented out b/c it can't
+	// (or at least shouldn't!) be used on itself.
+
 	// @Interceptors({LoggingInterceptor.class,LoggingInterceptor.class})
 	public void validateCredit() {
 		System.out.println("LoggingInterceptor.validateCredit(): I was here");
