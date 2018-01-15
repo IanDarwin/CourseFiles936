d9ead3ae49d7b67a356e62d12b0c2382d480cbae
Ian Darwin <ian@darwinsys.com>
2018-01-15 14:32:17 -0500

    pastos

--- configs/pg-meets-wf.sh
+++ configs/pg-meets-wf.sh
@@ -8,6 +8,6 @@ ftp https://jdbc.postgresql.org/download/postgresql-#{V}.jar
 cd /wildfly*/wildfly/bin
 ./jboss-cli.sh <<!
 connect
-module add –name=org.postgres –resources=/tmp/postgresql-${V}.jar –dependencies=javax.api,javax.transaction.api
+module add -name=org.postgres -resources=/tmp/postgresql-${V}.jar -dependencies=javax.api,javax.transaction.api
 /subsystem=datasources/jdbc-driver=postgres:add(driver-name=”postgres”,driver-module-name=”org.postgres”,driver-class-name=org.postgresql.Driver)
 !
