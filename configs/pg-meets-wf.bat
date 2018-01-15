84e93c0448f4011108d2a53252a155c8da9ded5f
Ian Darwin <ian@darwinsys.com>
2018-01-15 14:23:46 -0500

    MS-DOG version of same

---
+++ configs/pg-meets-wf.bat
@@ -0,0 +1,8 @@
+REM hookup postgresql into wildfly
+
+cd /wildfly*/bin
+jboss-cli.bat
+connect
+module add –name=org.postgres –resources=F:postgresql-9.4.1207.jar –dependencies=javax.api,javax.transaction.api
+/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)
+\q
