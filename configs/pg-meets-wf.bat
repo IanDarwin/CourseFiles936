8d028bd962d7bb37bdab21d3dcf5730534b4935d
Ian Darwin <ian@darwinsys.com>
2018-01-15 14:44:40 -0500

    Sigh, double dashes

--- configs/pg-meets-wf.bat
+++ configs/pg-meets-wf.bat
@@ -4,6 +4,6 @@ cd \wildfly*
 cd bin
 jboss-cli.bat
 connect
-module add -name=org.postgres -resources=/Users/student/Download/postgresql-9.4.1207.jar -dependencies=javax.api,javax.transaction.api
+module add --name=org.postgres --resources=/Users/student/Download/postgresql-9.4.1207.jar --dependencies=javax.api,javax.transaction.api
 /subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)
 \q
