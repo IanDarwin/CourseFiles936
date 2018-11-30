56d6dab40eed436985296fe08a4f8b66226e5ea3
Ian Darwin <ian@darwinsys.com>
2018-11-30 11:50:42 -0500

    Do timings, using Runnable Lambdas.

--- sourcecode/ex71solution/src/test/java/com/ticketmanor/service/EventsRestIT.java
+++ sourcecode/ex71solution/src/test/java/com/ticketmanor/service/EventsRestIT.java
@@ -7,6 +7,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -21,17 +22,25 @@ public class EventsRestIT {
 	private String URL2 =
 		"http://localhost:8080/ex71solution/rest/events2";
 	
+	Client cl;
+	
+	@Before
+	public void setup() {
+		cl = ClientBuilder.newClient();
+	}
+	
 	@Test
 	public void testGetEvents1() {
-		Client cl = ClientBuilder.newClient();
-		WebTarget target = cl.target(URL1);
-		@SuppressWarnings("unchecked")
-		List<Event> e = target.request(MediaType.APPLICATION_JSON).get(List.class);
-		//T Assert that the list contains at least one event
-		//-
-		System.out.printf("Got a list of %d events%n", e.size());
-		assertTrue(e.size() > 0);
-		//+
+		time("Events1", () -> {
+			WebTarget target = cl.target(URL1);
+			@SuppressWarnings("unchecked")
+			List<Event> e = target.request(MediaType.APPLICATION_JSON).get(List.class);
+			//T Assert that the list contains at least one event
+			//-
+			System.out.printf("Got a list of %d events%n", e.size());
+			assertTrue(e.size() > 0);
+			//+
+		});
 	}
 	
 	//T AFTER you build the "Indirect" version (EventsResource), annotate this to be a Test
@@ -39,15 +48,16 @@ public class EventsRestIT {
 	@Test
 	//+
 	public void testGetEvents2() {
-		Client cl = ClientBuilder.newClient();
-		WebTarget target = cl.target(URL2);
-		@SuppressWarnings("unchecked")
-		List<Event> e = target.request(MediaType.APPLICATION_JSON).get(List.class);
-		//T Prove that the list contains at least one event
-		//-
-		System.out.printf("Got a list of %d events%n", e.size());
-		assertTrue(e.size() > 0);
-		//+
+		time("Events2", () -> {
+			WebTarget target = cl.target(URL2);
+			@SuppressWarnings("unchecked")
+			List<Event> e = target.request(MediaType.APPLICATION_JSON).get(List.class);
+			//T Prove that the list contains at least one event
+			//-
+			System.out.printf("Got a list of %d events%n", e.size());
+			assertTrue(e.size() > 0);
+			//+
+		});
 	}
 
 	void time(String descr, Runnable r) {
