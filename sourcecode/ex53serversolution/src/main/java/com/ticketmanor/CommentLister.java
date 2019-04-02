e1aae4293a54598395e00f150665c23b576215b1
Ian Darwin <ian@darwinsys.com>
2019-04-02 15:30:22 -0400

    JPA Feedback lister for JSF use.

---
+++ sourcecode/ex53serversolution/src/main/java/com/ticketmanor/CommentLister.java
@@ -0,0 +1,19 @@
+package com.ticketmanor;
+
+import java.util.List;
+
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.ticketmanor.model.FeedbackForm;
+
+@Named
+public class CommentLister {
+
+	@PersistenceContext EntityManager em;
+	
+	public List<FeedbackForm> list() {
+		return em.createQuery("from FeedbackForm ff order by ff.date desc", FeedbackForm.class).getResultList();
+	}
+}
