513a7ee9947a59ef073cacae0509f25f91c7387f
Ian Darwin <ian@darwinsys.com>
2019-04-03 11:51:26 -0400

    CommentLister @SessionScoped (which requires Serializable)

--- sourcecode/ex53serversolution/src/main/java/com/ticketmanor/CommentLister.java
+++ sourcecode/ex53serversolution/src/main/java/com/ticketmanor/CommentLister.java
@@ -1,7 +1,9 @@
 package com.ticketmanor;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -9,7 +11,9 @@ import javax.persistence.PersistenceContext;
 import com.ticketmanor.model.FeedbackForm;
 
 @Named
-public class CommentLister {
+@SessionScoped
+public class CommentLister implements Serializable {
+	private static final long serialVersionUID = -8054003420102880724L;
 
 	@PersistenceContext EntityManager em;
 	
