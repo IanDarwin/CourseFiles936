39f210948383e3a21959e80c488557d629bce665
Ian Darwin <ian@darwinsys.com>
2019-04-06 17:02:32 -0400

    Convert MicroService from Signup to Customer Comment form...
    to become the UI for ex52's JMS-based comment/feedback service.
    Bonus is to actually send it.
    Also, sanitize input used in HTML feedback page.

--- sourcecode/exUSsolution/src/main/java/com/ticketmanor/microservice/MicroService.java
+++ sourcecode/exUSsolution/src/main/java/com/ticketmanor/microservice/MicroService.java
@@ -1,21 +1,37 @@
 package com.ticketmanor.microservice;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+
+import com.ticketmanor.model.FeedbackForm;
+
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 /**
  * CDI Bean
  */
-@Named
+@Named @RequestScoped
 public class MicroService {
 
 	String firstName, lastName, email;
+	String comments;
 	String greeting;
 	
-	public String signup() {
-		System.out.printf("MicroService.signup(%s)\n", firstName + ' ' + lastName);
+	public String submit() {
+		System.out.printf("MicroService.submit(%s)\n", firstName + ' ' + lastName);
 
-		// Here would be some code to save the signup in a database
+		// Here would be some code to send the information to TicketManor
+		// First we construct a FeedbackForm (from the datamodel project)
+		FeedbackForm form = 
+			new FeedbackForm(0, getName(), email, LocalDateTime.now(), comments);
+
+		//T BONUS ONLY Copy all the JMS code from the JMS Client exercise
+		// This is basically all the code in both try/catch statements
+		// But not the form construction (which we've done for you using the
+		// full version of the constructor), and you should also DELETE the 
+		// bogus "second" send of an invalid object.
+		// Near the end of the copied code, use JMS to send the form to TicketManor via JMS.
 
 		// Here is some display logic
 		String dayPart = dayPart(LocalTime.now().getHour());
@@ -37,7 +53,9 @@ public class MicroService {
 		return firstName;
 	}
 
+	/** Set the first name. MUST sanitize to prevent script kiddie HTML attacks */
 	public void setFirstName(String firstName) {
+		firstName = firstName.replaceAll("[^a-zA-Z ']", "");
 		this.firstName = firstName;
 	}
 
@@ -47,6 +65,14 @@ public class MicroService {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	/** We want first and last name separate; TicketManor wants a single name.
+	 * Merge two into one, and we're done!
+	 * @return The full name
+	 */
+	public String getName() {
+		return firstName + ' ' + lastName;
 	}
 
 	public String getEmail() {
@@ -59,5 +85,13 @@ public class MicroService {
 
 	public String getGreeting() {
 		return greeting;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
 	}
 }
--- sourcecode/exUSsolution/src/test/java/exUSsolution/MicroServiceTest.java
+++ sourcecode/exUSsolution/src/test/java/exUSsolution/MicroServiceTest.java
@@ -9,6 +9,19 @@ import com.ticketmanor.microservice.MicroService;
 public class MicroServiceTest {
 
 	@Test
+	public void testFirstnameSanitize() {
+		MicroService target = new MicroService();
+		// xkcd.com/327
+		target.setFirstName("Robert'; Drop table students; --");
+		assertEquals("name sanitize I",
+			"Robert' Drop table students ", target.getFirstName());
+
+		target.setFirstName("Robert<b><script>window.alert('Foo')");
+		assertEquals("name sanitize II",
+			"Robertbscriptwindowalert'Foo'", target.getFirstName());
+	}
+
+	@Test
 	public void testDayPart() {
 		assertEquals("Morning", MicroService.dayPart(0));
 		assertEquals("Morning", MicroService.dayPart(8));
