2b5be9b9696b5250e0f3fad04571101921c126f2
Ian Darwin <ian@darwinsys.com>
2019-04-07 19:19:58 -0400

    Update list of repos

--- scripts/populate_repos_936.sh
+++ scripts/populate_repos_936.sh
@@ -1,10 +1,16 @@
+#!/bin/sh
+
+# ONLY USE ONCE to init all the git repos
+
 cd /c
-for repo in cdi-ee-examples darwinsys-api clublist CourseFiles936 javasrc jsfdemo jpademo makehandsons
-do
-	git clone https://IanDarwin@github.com/IanDarwin/${repo}
-done
 
 for repo in TicketManorJava
 do
 	git clone https://LearningTree@github.com/LearningTree/${repo}
+done
+
+# We omit CourseFiles936 as you've obviously already got a copy
+for repo in makehandsons CreateProjects cdi-ee-examples ClubList javasrc darwinsys-api jsfdemo jpademo
+do
+	git clone https://IanDarwin@github.com/IanDarwin/${repo}
 done
