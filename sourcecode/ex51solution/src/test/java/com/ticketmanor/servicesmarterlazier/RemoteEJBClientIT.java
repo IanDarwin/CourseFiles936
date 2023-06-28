435f2a085ccd322daf3f7ff93ab507493a1733c5
Ian Darwin <ian@darwinsys.com>
2023-06-27 20:53:22 -0400

    Fix imports and missing field.

--- sourcecode/ex51solution/src/test/java/com/ticketmanor/servicesmarterlazier/RemoteEJBClientIT.java
+++ sourcecode/ex51solution/src/test/java/com/ticketmanor/servicesmarterlazier/RemoteEJBClientIT.java
@@ -2,6 +2,8 @@ package com.ticketmanor.servicesmarterlazier;
 
 import javax.naming.InitialContext;
 
+import com.ticketmanor.service.CreditCardValidateTest;
+import com.ticketmanor.service.CreditValidateInterface;
 import org.junit.Before;
 
 public class RemoteEJBClientIT extends CreditCardValidateTest {
@@ -10,12 +12,14 @@ public class RemoteEJBClientIT extends CreditCardValidateTest {
 		super(input, expected);
 	}
 
+	CreditValidateInterface testSubject;
+
 	private static final String LOOKUP_STRING = 
 		"ex51-1.0.0-SNAPSHOT/CreditCardValidate!com.ticketmanor.service.CreditValidateInterface";
 
 	@Before @Override
 	public void setUp() throws Exception {
-		testSubject = (CreditValidateInterface) 
+		testSubject = (CreditValidateInterface)
 				new InitialContext().lookup(LOOKUP_STRING);
 	}
 
